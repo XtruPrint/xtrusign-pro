246f4fa90b137e4f9b8586337ed897a442be894f
XtruPrint <mail@xtruprint.com>
2026-03-03 23:08:16 +0530

    Update test_geometry.cpp

--- test/test_geometry.cpp
+++ test/test_geometry.cpp
@@ -1,1 +1,9 @@
+#include <catch2/catch_test_macros.hpp>
+#include "GeometryEngine.h"
 
+TEST_CASE("Engine initializes correctly")
+{
+    GeometryEngine engine;
+
+    REQUIRE(engine.isInitialized() == true);
+}
